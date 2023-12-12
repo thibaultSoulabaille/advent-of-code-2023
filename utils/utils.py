1b63e4daec1459132d3be357f418a8000cba3186
thibaultSoulabaille <thibault.soulabaille@gmail.com>
2023-12-12 19:34:58 +0100

    Use requests to save input file

--- utils/utils.py
+++ utils/utils.py
@@ -1,8 +1,31 @@
+import json
+import requests
 from typing import Any
+
+
+def get_input(day: int) -> str:
+    with open("../config/get_input_cookies.json") as cookies_file:
+        cookies = json.load(cookies_file)
+
+    req = requests.get(
+        url=f"https://adventofcode.com/2023/day/{day}/input",
+        cookies=cookies
+    )
+
+    if req.status_code == 200:
+        return req.text
+    return ""
+
+
+def save_input_file(day: int) -> None:
+    input_content = get_input(day)
+    with open(f"../input/day_{day}.txt", "w+") as out_file:
+        out_file.write(input_content)
 
 
 def open_input(day: int, test: bool = False, as_list: bool = True) -> Any:
     if test:
+        save_input_file(day)
         f_name = f'input/test/day_{day}_test.txt'
     else:
         f_name = f'input/day_{day}.txt'
